a41bcd3a05258abf278d51bc75601070c9491535
Umer Ali <acad.aktidha@gmail.com>
2026-04-20 14:06:47 +0500

    url updated

--- src/pages/Enroll.tsx
+++ src/pages/Enroll.tsx
@@ -46,7 +46,7 @@ const Enroll = () => {
     setErrorMsg("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/contact", {
+      const res = await fetch("http://launchpad-to-expert-main-pl1n.vercel.app/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
